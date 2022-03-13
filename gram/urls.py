0ac4478dece1872574b20a8fcbfd0405a69ca241
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-13 10:30:48 +0300

    add base template for registration

--- gram/urls.py
+++ gram/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url,include
+from django.urls import path,include
 from django.contrib.auth import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url('^admin/', admin.site.urls),
-    url(r'', include('instagram.urls'))
+    path('admin/', admin.site.urls),
+    path('', include('instagram.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
 ]
