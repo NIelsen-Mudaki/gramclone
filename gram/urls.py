d5c7caf10a08029ce33347f685ba5bdd5f7532ef
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-15 00:15:48 +0300

    deploy to heroku

--- gram/urls.py
+++ gram/urls.py
@@ -23,7 +23,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('instagram.urls')),
-    path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('django_registration.backends.simple.urls')),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='index.html'), name='logout'),
 ]
