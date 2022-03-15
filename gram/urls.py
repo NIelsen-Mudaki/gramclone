8747048d808e03dd25592664da222857868d1b3a
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-15 17:23:31 +0300

    deploy to heroku

--- gram/urls.py
+++ gram/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('', include('instagram.urls')),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('login/', LoginView.as_view(template_name='django_registration/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='index.html'), name='logout'),
 ]
