2df499cc0e44a68a346fdf5c186c5b5e837882ef
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-23 15:21:41 +0300

    deploy to heroku

--- instagram/urls.py
+++ instagram/urls.py
@@ -9,6 +9,8 @@ urlpatterns=[
     url(r'^accounts/profile/', views.profile, name='profile'),
     url(r'^upload/', views.upload, name='upload'),
     url(r'^comment/$', views.comment, name='comment'),
+    url(r'^login/', views.login(template_name='registration/login.html'), name='login'),
+    url(r'^logout/', views.logout(template_name='index.html'), name='logout'),
 ]
 if settings.DEBUG:
 	urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
